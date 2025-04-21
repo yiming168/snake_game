0f905860372f131e9a5db30b01a722842536f17e
Yiming Zhu <yiming168@gmail.com>
2025-04-21 15:16:18 -0700

    add sanke class and food class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,27 +8,19 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-segments = []
+snake = Snake()
 
-for i in range(3):
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto((i*(-20),0))
-    segments.append(new_segment)
-
-
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg_num in range(len(segments)-1, 0, -1):
-        new_x=segments[seg_num -1].xcor()
-        new_y=segments[seg_num -1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-    segments[0].left(90)
+    snake.move()
 
 
 
---
+++ snake.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+NORTH = 90
+SOUTH = 270
+WEST = 180
+EAST = 0
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        for i in range(3):
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto((i * (-MOVE_DISTANCE), 0))
+            self.segments.append(new_segment)
+
+    def move(self):
+        for segment in range(len(self.segments)-1, 0, -1):
+            new_x = self.segments[segment-1].xcor()
+            new_y = self.segments[segment-1].ycor()
+            self.segments[segment].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.segments[0].heading() == SOUTH:
+            return
+        self.segments[0].setheading(NORTH)
+
+    def down(self):
+        if self.segments[0].heading() == NORTH:
+            return
+        self.segments[0].setheading(SOUTH)
+
+
+    def left(self):
+        if self.segments[0].heading() == EAST:
+            return
+        self.segments[0].setheading(WEST)
+
+    def right(self):
+        if self.segments[0].heading() == EAST:
+            return
+        self.segments[0].setheading(EAST)
